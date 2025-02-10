7d765135fa7a913f8aceddb80a472bf1824e566b
ItssPiyush <itsspiyush@gmail.com>
2025-02-10 17:39:28 +0530

    authentication logic

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import db from "../db.js";
+import { send } from "process";
 
 const router = express.Router();
 
@@ -19,7 +20,7 @@ router.post("/register", (req, res) => {
     //now that we have a user, I want to add their first todo for them
     const defaultTodo = `Hello :) Add your first todo!`;
     const insertTodo = db.prepare(
-      `INSERT INTO todo(user_id, task) VALUES(?,?)`
+      `INSERT INTO todos(user_id, task) VALUES(?,?)`
     );
     insertTodo.run(result.lastInsertRowid, defaultTodo);
 
@@ -27,8 +28,9 @@ router.post("/register", (req, res) => {
     const token = jwt.sign(
       { id: result.lastInsertRowid },
       process.env.JWT_secret,
-      { expiresIN: "24h" }
+      { expiresIn: "24h" }
     );
+    res.json({ token });
   } catch (err) {
     console.log(err.message);
     res.sendStatus(503);
@@ -36,7 +38,29 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // const
+  const { username, password } = req.body;
+
+  try {
+    const getUser = db.prepare("SELECT * FROM users WHERE username = ?");
+    const user = getUser.get(username);
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    if (!passwordIsValid) {
+      return res.status(401).send({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    res.json({ token });
+  } catch (err) {
+    console.log(err.message);
+    res.sendStatus(503);
+  }
 });
 
 export default router;
