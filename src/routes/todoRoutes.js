8a0885991037b8a7962451d5579511f5662051bc
ItssPiyush <itsspiyush@gmail.com>
2025-02-11 20:57:24 +0530

    complete

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "../db.js";
+import { userInfo } from "os";
 
 const router = express.Router();
 
@@ -23,9 +24,24 @@ router.post("/", (req, res) => {
 });
 
 //update a todo
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  const { completed } = req.body;
+  const { id } = req.params;
+  const { page } = req.query;
+
+  const updatedTodo = db.prepare("UPDATE todos SET completed = ? WHERE id = ?");
+  updatedTodo.run(completed, id);
+  res.json({ message: "Todo Completed!" });
+});
 
 //delete a todo
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const userId = req.userId;
+
+  const deleteTodo = db.prepare("DELETE FROM todos WHERE id = ? AND user_id");
+  deleteTodo.run(id, userId);
+  res.json({ message: "Todo Deleted!" });
+});
 
 export default router;
